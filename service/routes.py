6f28105954a653589775ad8dc75a69a3fb8d4837
Fabio Missio <fabiomix@users.noreply.github.com>
2023-05-01 05:10:48 -0400

    add update_account and its tests

--- service/routes.py
+++ service/routes.py
@@ -85,7 +85,19 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account {account_id} not found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
